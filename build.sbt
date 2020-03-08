0df3fe2d56f543a477548928d0c978f3ec23f15b
pxfnc <dev.pxfnc@gmail.com>
2020-03-08 16:27:24 +0900

    initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+
+name := "elastiCounter"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies += Dependencies.akkaActor
---
+++ project/Dependencies.scala
@@ -0,0 +1,9 @@
+import sbt._
+
+object Dependencies {
+
+  // https://mvnrepository.com/artifact/com.typesafe.akka/akka-actor
+  lazy val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.6.3"
+
+
+}
