a7a3a177bdd45b8a4e7b610a9822cfe5c2f68a51
sdual <shr.nrht.0623@gmail.com>
2020-03-22 03:31:17 +0900

    don't use deprecated ActorMaterializer

--- src/main/scala/com/serviveragent/erasticounter/Main.scala
+++ src/main/scala/com/serviveragent/erasticounter/Main.scala
@@ -5,7 +5,7 @@ import akka.actor.typed.scaladsl.adapter._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.server.Route
-import akka.stream.ActorMaterializer
+import akka.stream.Materializer
 import com.serviveragent.erasticounter.router.RestRoute
 
 import scala.concurrent.Future
@@ -21,7 +21,7 @@ object Main extends App {
   private def runRoute(): Unit = {
     implicit val untypedSystem: actor.ActorSystem = AkkaTypedImplicits.system.toClassic
     implicit val ec                               = untypedSystem.dispatcher
-    implicit val materializer                     = ActorMaterializer()(untypedSystem) // TODO: don't use deprecated method.
+    implicit val materializer                     = Materializer(untypedSystem)
 
     val route: Route                         = new RestRoute().allRoutes
     val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(route, host, port)
